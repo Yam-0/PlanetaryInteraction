ed4e918beeb3de968c7764dc2dc56f5ad9a81efb
Yam-0 <tage.akerstrom@gmail.com>
2020-02-11 23:09:42 +0100

    Work on gravity formula

--- out/source/PlanetaryInteraction.java
+++ out/source/PlanetaryInteraction.java
@@ -18,20 +18,29 @@ PImage rocketImage;
 
 PVector speed;
 PVector position;
+PVector rocketCenterOfMass;
 PVector addSpeed;
+PVector speedToStar;
 PVector rocketScale;
 
 float thrusterStrength;
+float gravityStrength;
 float rotationSpeed;
 float heading;
 float angle;
+float angleToStar;
+float distanceToStar;
 float maxSpeed;
 float centerOffset;
+float playfield;
 
 float addspeedX;
 float addspeedY;
+float speedToStarX;
+float speedToStarY;
 
 boolean up, down, left, right;
+boolean debugView;
 
 public void setup() {
 	//load rocket.png image 
@@ -41,32 +50,47 @@ public void setup() {
 	
 	
 	//vector2 variables
-	speed = new PVector(0, 0);
-	position = new PVector(512, 512);
+	speed = new PVector(0, 2);
+	position = new PVector(512, 700);
 	addSpeed = new PVector(0, 0);
 	rocketScale = new PVector(25, 50);
-
+	speedToStar = new PVector(0, 0);
+	
 	//positional variables
-	thrusterStrength = 0.05f;
-	rotationSpeed = 2;
+	distanceToStar = 0;
+	thrusterStrength = 0.003f;
+	gravityStrength = 0.05f;
+	rotationSpeed = 2.5f;
 	heading = 0;
 	angle = 0;
-	maxSpeed = 5;
+	maxSpeed = 3;
 	centerOffset = 15;
+	angleToStar = 0;
+	playfield = 800;
 
 	//input booleans
 	up = down = left = right = false;
+	debugView = false;
 }
 
 public void draw() {
+
+	//get distance to star
+	distanceToStar = getDistance(position.x, position.y);
+	if(distanceToStar <= 5)
+	{
+		reset();
+		return;
+	}
 
 	//update angle on input
 	if(right == true) angle += rotationSpeed;
 	if(left == true) angle -= rotationSpeed;
 
+	//get "forward" speed vector2
 	if(up == true)
 	{
-		//angle to vector2
+		//get vector2 from angle
 		addspeedX = (float)Math.cos(radians(angle));
 		addspeedY = (float)Math.sin(radians(angle));
 
@@ -74,13 +98,40 @@ public void draw() {
 		addSpeed.x = (float)Math.round(addspeedX * 100) / 100;
 		addSpeed.y = (float)Math.round(addspeedY * 100) / 100;
 
-		//print for debugging
-		//println(addSpeed);
-
 		//apply to speed
 		speed.x += addSpeed.x * thrusterStrength;
 		speed.y += addSpeed.y * thrusterStrength;
 	}
+
+	//get vector2 speeds towards star
+	//get angle to star
+	rocketCenterOfMass = new PVector(position.x, position.y);
+	angleToStar = getAngle(rocketCenterOfMass, new PVector(512, 512));
+	float angleToStarOffset = angleToStar - 90;
+	//println("angleToStar: "+ angleToStar);
+
+	//angle to vector2
+	speedToStarX = (float)Math.cos(radians(angleToStarOffset));
+	speedToStarY = (float)Math.sin(radians(angleToStarOffset));
+
+	//round addSpeed to two decimal places
+	speedToStar.x = (float)Math.round(speedToStarX * 100) / 100;
+	speedToStar.y = (float)Math.round(speedToStarY * 100) / 100;
+
+	//calculate gravity scale
+	gravityStrength = (float)(4 * (1000)/(Math.pow(distanceToStar, 2)));
+	//increases gravity if outside playing field
+	if(distanceToStar > playfield/2){ 
+		gravityStrength = 0.2f;
+	}
+
+	//apply to speed
+	speed.x += (speedToStar.x * gravityStrength);
+	speed.y += (speedToStar.y * gravityStrength);
+
+	//round speed
+	speed.x = (float)Math.round(speed.x * 100) / 100;
+	speed.y = (float)Math.round(speed.y * 100) / 100;
 	
 	//limit speed
 	if(speed.x > maxSpeed)
@@ -91,13 +142,14 @@ public void draw() {
 	{
 		speed.y = maxSpeed;
 	}
-	
-	//round speed
-	speed.x = (float)Math.round(speed.x * 100) / 100;
-	speed.y = (float)Math.round(speed.y * 100) / 100;
-	
-	//print for debugging
-	//println(speed);
+	if(speed.x < -maxSpeed)
+	{
+		speed.x = -maxSpeed;
+	}
+	if(speed.y < -maxSpeed)
+	{
+		speed.y = -maxSpeed;
+	}
 
 	//apply speed
 	position.x += speed.y;
@@ -106,17 +158,96 @@ public void draw() {
 	//set background color
 	background(0);
 
+	//star and play area
+	fill(5, 5, 5);
+	ellipse(512, 512, playfield, playfield);
+	fill(255, 255, 0);
+	ellipse(512, 512, 25, 25);
+
 	//update position and rotation
 	pushMatrix();
 	translate(position.x, position.y);
 	rotate(radians(angle));
 	image(rocketImage, (-rocketScale.x)/2, (-rocketScale.y)/2 - centerOffset, rocketScale.x, rocketScale.y);
 	popMatrix();
+
+	heading = getAngleXY(speed.x, speed.y);
+
+	if(debugView)
+	{
+		//show center off mass
+		fill(255, 0, 0);
+		ellipse(rocketCenterOfMass.x, rocketCenterOfMass.y, 10, 10);
+		//print for debugging
+		//println(rocketCenterOfMass);
+		
+		//draw line to star
+		pushMatrix();
+		translate(512, 512);
+		rotate(radians(angleToStar));
+		rect(0, -1, 1000, 2);
+		popMatrix();
+
+		//draw movement direction
+		pushMatrix();
+		translate(rocketCenterOfMass.x, rocketCenterOfMass.y);
+		rotate(radians(heading));
+		fill(0, 0, 255);
+		rect(0, -1, 100, 2);
+		popMatrix();
+
+		println("gravityStrength: "+ gravityStrength + " | distanceToStar: "+ distanceToStar + " | " + position);
+	}
 }
 
+public float getDistance(float x, float y)
+{
+	//pythagorean theorem
+	float distance = (float)(Math.sqrt(Math.pow((x - 512), 2) + Math.pow((y - 512), 2)));
+	return distance;
+}
+public float getAngleXY(float x, float y)
+{
+	//arctan and rad to degrees - to get angle with x and y
+    float deg = (float)(Math.atan2(y, x)*180.0f/Math.PI);
+	deg -= 90;
+	return deg;
+}
+
+public float getAngle(PVector target, PVector here) {
+
+	//arctan and rad to degrees - to get angle between points
+    float angle = (float) Math.toDegrees(Math.atan2(target.y - here.x, target.x - here.y));
+
+    if(angle < 0){
+        angle += 360;
+    }
+
+    return angle;
+}
 //key held down variable updates
 public void keyPressed()
 {
+	//turn on/off debug mode
+	if(key == '§')
+	{
+		if(debugView == false)
+		{
+			debugView = true;
+		}
+		else 
+		{
+			debugView = false;
+		}
+	}
+	//reset button for debug mode
+	if(key == '+')
+	{
+		if(debugView == true)
+		{
+			reset();
+		}
+	}
 	if (key == CODED) 
 	{
 		switch(keyCode) {
@@ -200,6 +331,35 @@ public void keyReleased()
 				break;
 		}
 	}
+}
+public void reset(){
+	//reset every variable except debug
+	rocketImage = loadImage("Rocket.png");
+
+	//window size
+	size(1024, 1024);
+	
+	//vector2 variables
+	speed = new PVector(0, 2);
+	position = new PVector(512, 700);
+	addSpeed = new PVector(0, 0);
+	rocketScale = new PVector(25, 50);
+	speedToStar = new PVector(0, 0);
+	
+	//positional variables
+	distanceToStar = 0;
+	thrusterStrength = 0.003f;
+	gravityStrength = 0.05f;
+	rotationSpeed = 2.5f;
+	heading = 0;
+	angle = 0;
+	maxSpeed = 3;
+	centerOffset = 15;
+	angleToStar = 0;
+	playfield = 800;
+
+	//input booleans
+	up = down = left = right = false;
 }
   public void settings() { 	size(1024, 1024); }
   static public void main(String[] passedArgs) {
